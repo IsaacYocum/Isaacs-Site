8ab5c8c7ee3d617e9839de5c0ddf6d9498afcd47
Isaac <isaac@Hazels-MacBook-Pro.local>
2018-05-22 19:11:26 -0600

    Added style.css and a button showing the current date.

--- hello.js
+++ hello.js
@@ -1,1 +1,12 @@
 console.log("Hello, world!", "how's it going?");
+
+const daysOfTheWeek = ["Sunday", "Monday", "Tuesday", "Wednesday",
+                       "Thursday", "Friday", "Saturday"];
+
+
+let now = new Date();
+let dayName = daysOfTheWeek[now.getDay()];
+
+function displayDate() {
+  document.getElementById("day").innerHTML = dayName + " " + now.getDate() + ", " + now.getFullYear();
+}
