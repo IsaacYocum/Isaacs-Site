b6882dbdeb07f52cc876801343bb8071092cefcb
Isaac <isaac@Hazels-MacBook-Pro.local>
2018-05-23 18:33:57 -0600

    Rearranged square calculation buttons. The square function can now handle empty string correctly.

--- square.js
+++ square.js
@@ -10,14 +10,23 @@ let squared = (x) => (x*x);
 console.log(square(2));
 console.log(squared(4));
 
+//Takes input, converts to float, and outputs it's square (x^2)
 function square(x) {
   var x = document.getElementById('squareInput').value;
 
-  //Checks if x can be parsed into a float. If true -- calculate, false -- display hint 
+  //Checks if x can be parsed into a float. If true -- calculate, if false -- display hint, if empty -- do nothing
   if (parseFloat(x)) {
     var answer = Math.pow(parseFloat(x), 2);
     document.getElementById('squareOutput').value = answer;
+  } else if (document.getElementById('squareInput').value === "" && document.getElementById('squareOutput').value === "") {
+    break;
   } else {
     document.getElementById('squareOutput').value = "Numbers only";
   }
+}
+
+//Clears contents in the squareInput and squareOutput textboxes
+function clearSquare() {
+  document.getElementById('squareInput').value = "";
+  document.getElementById('squareOutput').value = "";
 }
